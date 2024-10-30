ac5960cf6bfca98a7551c00411f75bfabadfa4e0
Luuk Muskens <luuk@travpromobile.com>
2024-10-30 11:39:00 +0100

    Add files via upload

---
+++ src/kiss.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = ({ text, buttonText }) => {
+  return (
+    <div className="card">
+      <p>{text}</p>
+      <button>{buttonText}</button>
+    </div>
+  );
+};
+
+export default Card;
